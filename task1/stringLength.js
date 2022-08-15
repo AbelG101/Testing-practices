7bbe21f970ac0f667e387010eb8fe5a8b0e045b1
AbelG101 <abelgebeyehu101@gmail.com>
2022-08-15 23:07:22 +0300

    add character count inplementations

--- task1/stringLength.js
+++ task1/stringLength.js
@@ -1,5 +1,9 @@
 const stringLength = string => {
-  return string.length;
+  if (string.length > 0 && string.length <11) {
+    return string.length
+  } else {
+    throw new Error('String cant be empty');
+  }
 };
 
 module.exports = stringLength
